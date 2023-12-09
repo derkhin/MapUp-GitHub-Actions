6dbe441217921b786103098642e652489ce3bb5d
Irfan Sayyed <irfan@saaragh.in>
2023-12-09 18:41:12 +0530

    testing pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     return (
         <>
             <div>
-                <div style={{fontSize:'80px',fontWeight:'bold'}}>MapUp-GitHub-Actions</div>
+                <div style={{fontSize:'80px',fontWeight:'bold'}}>MapUp-GitHub-Actions : ID #2</div>
                 <span style={{fontSize:'40px',fontWeight:'normal'}}>Hello World</span>
             </div>
         </>
