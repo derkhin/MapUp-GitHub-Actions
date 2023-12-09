9083d5b494c6b37c1d556da14d78213ff7a76d5d
Irfan Sayyed <irfan@saaragh.in>
2023-12-09 09:47:30 +0530

    main: Create a Simple React App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div>
+                <div style={{fontSize:'80px',fontWeight:'bold'}}>MapUp-GitHub-Actions</div>
+                <span style={{fontSize:'40px',fontWeight:'normal'}}>Hello World</span>
+            </div>
+        </>
+    );
+}
+
+export default App;
